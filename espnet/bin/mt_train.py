e932b5c12b659103a9ae0604899bcfc374e388a5
Jxu-Thu <jxu3425@gmail.com>
2020-08-02 12:32:29 +0800

    modify for blob

--- espnet/bin/mt_train.py
+++ espnet/bin/mt_train.py
@@ -12,11 +12,16 @@ import random
 import subprocess
 import sys
 
+if "/home/espnet" in sys.path:
+    sys.path.remove("/home/espnet")
+ESPNET_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir))
+sys.path.insert(0, ESPNET_ROOT)
 from distutils.version import LooseVersion
 
 import configargparse
 import numpy as np
 import torch
+
 
 from espnet.utils.cli_utils import strtobool
 from espnet.utils.training.batchfy import BATCH_COUNT_CHOICES
