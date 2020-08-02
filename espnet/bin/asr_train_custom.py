eb617d8c2062db09a3a7e59f18df27ae1434b4ea
Jxu-Thu <jxu3425@gmail.com>
2020-08-02 12:50:34 +0800

    modify for blob

--- espnet/bin/asr_train_custom.py
+++ espnet/bin/asr_train_custom.py
@@ -16,6 +16,7 @@ if "/home/espnet" in sys.path:
     sys.path.remove("/home/espnet")
 ESPNET_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir))
 sys.path.insert(0, ESPNET_ROOT)
+# chmod
 
 from distutils.version import LooseVersion
 
