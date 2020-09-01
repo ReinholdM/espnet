af3e6a03e569f84269e4a27004017a5c5636d103
Jxu-Thu <jxu3425@gmail.com>
2020-09-01 23:21:20 +0800

    modify for blob

--- egs/librispeech/asr1/submit_run3.sh
+++ egs/librispeech/asr1/submit_run3.sh
@@ -116,14 +116,13 @@ mkdir -p ${expdir}
 
 
 #resume=${exp_prefix}/exp/train_960_pytorch_lh/results/model.loss.best       # Resume the training from snapshot
-ls -l -trl ${expdir}/results | grep snapshot. | tail -1 >s.tmp
-snap_num=$(awk -F ' ' '{print $NF}' s.tmp)
-resume=${expdir}/results/${snap_num}
+if [ -f ${expdir}/results ]; then
 
-if [ -f ${resume} ]; then
-   resume=${resume}
+    ls -l -trl ${expdir}/results | grep snapshot. | tail -1 >s.tmp
+    snap_num=$(awk -F ' ' '{print $NF}' s.tmp)
+    resume=${expdir}/results/${snap_num}
 else
-   resume=
+    resume=
 fi
 
 echo 'exp_dir'
